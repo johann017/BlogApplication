54e266904921036431ef83788f7c16173e697fe9
Johann Antisseril <57604319+johann017@users.noreply.github.com>
2021-11-03 10:11:54 -0400

    Add files via upload

---
+++ blog/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import get_object_or_404, render
+from django.views import generic
+
+from .forms import CommentForm
+from .models import Post
+
+
+class PostList(generic.ListView):
+    queryset = Post.objects.order_by("created_on")
+    template_name = "index.html"
+
+
+def post_detail(request, slug):
+    template_name = "post_detail.html"
+    post = get_object_or_404(Post, slug=slug)
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    return render(
+        request,
+        template_name,
+        {
+            "post": post,
+            "comments": comments,
+            "new_comment": new_comment,
+            "comment_form": comment_form,
+        },
+    )
